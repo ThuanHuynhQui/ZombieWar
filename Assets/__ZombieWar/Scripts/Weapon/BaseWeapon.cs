97c1eaf1e569ebf324ab9f64b07d157dead149be
ThuanHuynh <thuan.huynhqui@gmail.com>
2024-09-19 18:40:16 +0700

    - Feat: adding weapon pooling

--- Assets/__ZombieWar/Scripts/Weapon/BaseWeapon.cs
+++ Assets/__ZombieWar/Scripts/Weapon/BaseWeapon.cs
@@ -4,6 +4,60 @@ using UnityEngine;
 
 public class BaseWeapon : MonoBehaviour
 {
+    protected WeaponSO weaponSO;
+    public WeaponSO WeaponSO => weaponSO;
+    protected float weaponDamage => weaponSO ? weaponSO.WeaponDamage : 1;
+    protected int ammoEachMagazine => weaponSO ? weaponSO.AmmoEachMagazine : 1;
+    protected float projectileSpeed => weaponSO ? weaponSO.ProjectileSpeed : 50;
+    protected float fireRate => weaponSO ? weaponSO.FireRate : 1;
+    protected GameObject projectilePrefab => weaponSO ? weaponSO.ProjectilePrefab : null;
+
+    ObjectPooling<GameObject> projectilePooling;
+
+    protected ObjectPooling<GameObject> ProjectilePooling
+    {
+        get
+        {
+            if (projectilePooling == null) InitPooling();
+            return projectilePooling;
+        }
+    }
+
+    void InitPooling()
+    {
+        if (projectilePrefab == null) return;
+        projectilePooling = new ObjectPooling<GameObject>(
+                        instantiateMethod: InstantiateMethod,
+                        destroyMethod: DestroyMethod,
+                        resetMethod: ResetMethod,
+                        preAddToPool: PreAddToPool,
+                        preLeavePool: PreLeavePool
+                    )
+        {
+            PregenerateOffset = 5
+        };
+
+        GameObject InstantiateMethod()
+        {
+            return Instantiate(projectilePrefab, transform);
+        }
+        void DestroyMethod(GameObject projectile)
+        {
+            Destroy(projectile);
+        }
+        void ResetMethod(GameObject projectile)
+        {
+
+        }
+        void PreAddToPool(GameObject projectile)
+        {
+            projectile.SetActive(false);
+        }
+        void PreLeavePool(GameObject projectile)
+        {
+            
+        }
+    }
     /// <summary>
     /// Attack at specific direction
     /// </summary>
@@ -12,6 +66,7 @@ public class BaseWeapon : MonoBehaviour
     {
 
     }
+
     /// <summary>
     /// Attack at forward direction
     /// </summary>
